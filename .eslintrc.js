caafc41a3b71ef39ff85f7e6826487c491d5d2a2
Corentin <corentin.tho@gmail.com>
2021-03-11 21:18:14 +0100

    config: react+tailwind

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:jsx-a11y/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // React is no longer needed in scope
+    'react/prop-types': 'off',
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="bg-red-500 text-white text-center">Hello world!</div>
+  );
+}
+
+export default App;
