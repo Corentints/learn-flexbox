94e7dc2f8611e5c7ed9fc383bcb5581991e8bd6d
Corentin <corentin.tho@gmail.com>
2021-03-13 02:55:21 +0100

    config(tailwind): whitelist dynamics bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,21 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    options: {
+      safelist: [
+        'bg-pink-600',
+        'hover:bg-pink-100',
+        'bg-green-600',
+        'hover:bg-green-100',
+        'bg-blue-600',
+        'hover:bg-blue-100',
+        'bg-indigo-600',
+        'hover:bg-indigo-100',
+        'bg-yellow-600',
+        'hover:bg-yellow-100',
+      ],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
