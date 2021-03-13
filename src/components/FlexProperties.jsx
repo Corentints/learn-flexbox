93c6f764c2a3ad833d1dede1c14bcdda5e61401b
Corentin <corentin.tho@gmail.com>
2021-03-13 02:39:52 +0100

    feat: flex properties component

---
+++ src/components/FlexProperties.jsx
@@ -0,0 +1,50 @@
+import ButtonsGroup from './ButtonsGroup';
+
+const FlexProperties = ({
+  flexDirection, setFlexDirection, flexWrap, setFlexWrap, alignItems, setAlignItems, justifyContent,
+  setJustifyContent, alignContent, setAlignContent,
+}) => (
+  <div className="grid justify-between w-full grid-cols-3 gap-4 mb-4">
+    <ButtonsGroup
+      title="flex-direction"
+      color="pink"
+      propertiesName={['column', 'row']}
+      currentProperty={flexDirection}
+      setProperty={setFlexDirection}
+    />
+
+    <ButtonsGroup
+      title="flex-wrap"
+      color="green"
+      propertiesName={['wrap', 'wrap-reverse', 'nowrap']}
+      currentProperty={flexWrap}
+      setProperty={setFlexWrap}
+    />
+
+    <ButtonsGroup
+      title="align-items"
+      color="blue"
+      propertiesName={['flex-start', 'flex-end', 'center', 'stretch', 'baseline']}
+      currentProperty={alignItems}
+      setProperty={setAlignItems}
+    />
+
+    <ButtonsGroup
+      title="justify-content"
+      color="indigo"
+      propertiesName={['flex-start', 'flex-end', 'center', 'space-between', 'space-around', 'space-evenly']}
+      currentProperty={justifyContent}
+      setProperty={setJustifyContent}
+    />
+
+    <ButtonsGroup
+      title="align-content"
+      color="yellow"
+      propertiesName={['flex-start', 'flex-end', 'center', 'space-between', 'space-around', 'space-evenly']}
+      currentProperty={alignContent}
+      setProperty={setAlignContent}
+    />
+  </div>
+);
+
+export default FlexProperties;
