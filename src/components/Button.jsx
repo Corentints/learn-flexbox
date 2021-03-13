60f7a3a63c453419d4b465bac81ead66bd87057b
Corentin <corentin.tho@gmail.com>
2021-03-13 02:40:18 +0100

    feat: button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 const Button = ({
-  color, propertyName, propertyClassName, currentProperty, setProperty,
+  color, propertyName, currentProperty, setProperty, setOpened,
 }) => (
-  <button type="button" onClick={() => setProperty(propertyClassName)} className={`${currentProperty === propertyClassName ? `border-b-2 border-${color}-600 ` : ''}relative inline-flex items-center px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-${color}-600`}>
-    <span tabIndex="-1" className="focus:outline-none">{propertyName}</span>
+  <button type="button" onClick={() => { setProperty(propertyName); setOpened(false); }} className={`${currentProperty === propertyName ? `bg-${color}-100` : ''} hover:bg-${color}-100 relative w-full inline-flex items-center px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-${color}-600`}>
+    <span tabIndex="-1" className="mx-auto focus:outline-none">{propertyName}</span>
   </button>
 );
 
